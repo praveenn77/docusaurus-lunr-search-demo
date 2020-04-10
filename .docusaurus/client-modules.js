9fad7f4f22327da60dca50ec392fca876b912278
Praveen N <praveenn@visualbi.com>
2020-04-10 14:06:12 +0530

    Initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("infima/dist/css/default/default.css"),
+  require("remark-admonitions/styles/infima.css"),
+  require("D:\\J\\OfflineSearchDemo\\src\\css\\custom.css"),
+  require("D:\\J\\OfflineSearchDemo\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
+];
---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,104 @@
+export default {
+  "plugins": [
+    "docusurus-lunr-search"
+  ],
+  "themes": [],
+  "customFields": {},
+  "themeConfig": {
+    "navbar": {
+      "title": "My Site",
+      "logo": {
+        "alt": "My Site Logo",
+        "src": "img/logo.svg"
+      },
+      "links": [
+        {
+          "to": "docs/doc1",
+          "activeBasePath": "docs",
+          "label": "Docs",
+          "position": "left"
+        },
+        {
+          "to": "blog",
+          "label": "Blog",
+          "position": "left"
+        },
+        {
+          "href": "https://github.com/facebook/docusaurus",
+          "label": "GitHub",
+          "position": "right"
+        }
+      ]
+    },
+    "footer": {
+      "style": "dark",
+      "links": [
+        {
+          "title": "Docs",
+          "items": [
+            {
+              "label": "Style Guide",
+              "to": "docs/doc1"
+            },
+            {
+              "label": "Second Doc",
+              "to": "docs/doc2"
+            }
+          ]
+        },
+        {
+          "title": "Community",
+          "items": [
+            {
+              "label": "Stack Overflow",
+              "href": "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              "label": "Discord",
+              "href": "https://discordapp.com/invite/docusaurus"
+            }
+          ]
+        },
+        {
+          "title": "Social",
+          "items": [
+            {
+              "label": "Blog",
+              "to": "blog"
+            },
+            {
+              "label": "GitHub",
+              "href": "https://github.com/facebook/docusaurus"
+            },
+            {
+              "label": "Twitter",
+              "href": "https://twitter.com/docusaurus"
+            }
+          ]
+        }
+      ],
+      "copyright": "Copyright © 2020 My Project, Inc. Built with Docusaurus."
+    }
+  },
+  "title": "My Site",
+  "tagline": "The tagline of my site",
+  "url": "https://your-docusaurus-test-site.com",
+  "baseUrl": "/",
+  "favicon": "img/favicon.ico",
+  "organizationName": "facebook",
+  "projectName": "docusaurus",
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "sidebarPath": "D:\\J\\OfflineSearchDemo\\sidebars.js",
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/"
+        },
+        "theme": {
+          "customCss": "D:\\J\\OfflineSearchDemo\\src\\css\\custom.css"
+        }
+      }
+    ]
+  ]
+};
